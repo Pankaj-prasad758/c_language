78357327c79d06a48f94a81675a14a4cf03eae93
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-11-30 17:50:19 +0530

    Add sum.c and update variables.c main logic
    Introduced sum.c with examples of basic arithmetic and type conversion in C. Updated variables.c to prompt for a score input instead of a magic number, and commented out previous main function examples.

---
+++ sum.c
@@ -0,0 +1,42 @@
+# include<stdio.h>
+
+// int main(){
+//     int a, b;
+//     printf("Enter value of a and b ");
+//     scanf("%d %d", &a, &b);
+
+//     // printf("Enter value of b ");
+//     // scanf("%d", &b);
+
+//     // int sum = a + b ;
+//     printf("sum of a and b is : %d", a + b);
+//     return 0;
+
+// }
+
+//Area of Square
+// int main(){
+//     int a;
+//     printf("Enter value of a to calculate area of sqaure ");
+//     scanf("%d", &a);
+//     // int area = a * a;
+//     printf("Area of square is : %d", a * a);
+//     return 0;
+// }
+
+//Area of circle
+// int main(){
+//     float radius;
+//     printf("Enter radius");
+//     scanf("%f", &radius);
+//     printf("Area of circle is : %f", 3.14 * radius * radius);
+//     return 0;
+// }
+
+//Typpe conversion
+
+int main(){
+    int a = 4 * 3 / 6 * 2;
+    printf("Value of a : %d",a);
+    return 0;
+}
--- variables.c
+++ variables.c
@@ -21,14 +21,28 @@
 //     test();
 // }
 
-int main(){
-//    float realNumber = 3.14;
-//     printf("real number is %f\n", realNumber);
-//     return 0;
-int magic_number;
-printf("enter the magic number");
-scanf("%d", &magic_number);
-printf("magic number is %d", magic_number);
-return 0;
+// int main(){
+// //    float realNumber = 3.14;
+// //     printf("real number is %f\n", realNumber);
+// //     return 0;
+// int magic_number;
+// printf("enter the magic number");
+// scanf("%d", &magic_number);
+// printf("magic number is %d", magic_number);
+// return 0;
 
+// }
+
+// int main(){
+//    int primeNumber = 2;
+//     printf("value of prime Number %d\n",primeNumber);
+//     return 0;
+// }
+
+int main(){
+    int score;
+    printf("Enter your score");
+    scanf("%d", &score);
+    printf("your score is: %d", score);
+    return 0;
 }
