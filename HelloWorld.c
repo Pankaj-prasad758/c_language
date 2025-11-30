296d15724c5be5f61796279dbaa8587cf193c00b
Pankaj-prasad758 <prasadpankaj897@gmail.com>
2025-11-30 12:32:47 +0530

    basic_learning

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,5 +2,8 @@
 
 int main()  {
     printf("Hello my dear earth");
+    long long phoneNumber = 7778889;
+    char name = 't';
+    printf(" phone number \n %11d",phoneNumber, name);
     return 0;
 }
---
+++ variables.c
@@ -0,0 +1,34 @@
+# include<stdio.h>
+
+// void test() {
+
+// static int number = 20;
+// number++;
+// printf("%d\n", number);
+// }
+
+// test();
+
+// void test() {
+//     static int x = 10;
+//     x++;
+//     printf("%d\n", x);
+// }
+
+// int main() {
+//     test();
+//     test();
+//     test();
+// }
+
+int main(){
+//    float realNumber = 3.14;
+//     printf("real number is %f\n", realNumber);
+//     return 0;
+int magic_number;
+printf("enter the magic number");
+scanf("%d", &magic_number);
+printf("magic number is %d", magic_number);
+return 0;
+
+}
